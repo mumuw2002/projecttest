6d176aae9fe51699f607fbcfdb0478306a10e0a6
mumuw2002 <chanya.tho@kkumail.com>
2025-01-27 23:47:32 +0700

    log commit

--- server/controllers/mainController.js
+++ server/controllers/mainController.js
@@ -3,14 +3,22 @@
  * Homepage
 */
 exports.homepage = async (req, res) => {
-  const locals = {
-    title: "NodeJs Notes",
-    description: "Free NodeJS Notes App.",
+  try {
+    const users = await User.find({}); // ดึงข้อมูลผู้ใช้ทั้งหมดจากฐานข้อมูล
+
+    const locals = {
+      title: "NodeJs Notes",
+      description: "Free NodeJS Notes App.",
+    };
+    console.log(users); 
+    res.render('index', {
+      user: req.user,
+      users: users, // ส่งข้อมูลผู้ใช้ไปยัง view
+      locals,
+      layout: '../views/layouts/front-page'
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('เกิดข้อผิดพลาดภายในเซิร์ฟเวอร์');
   }
-  res.render('index', {
-    user: req.user,
-    users: users,
-    locals,
-    layout: '../views/layouts/front-page'
-  });
-}
+};
